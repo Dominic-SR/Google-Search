fa62fec24b283776b8cdd7b1dac70ae84a4fe982
dominic <dominicseduraja3@gmail.com>
2024-09-18 23:47:15 +0530

    avatar added

---
+++ src/Components/Avatar.tsx
@@ -0,0 +1,18 @@
+import React,{FC} from 'react'
+
+interface props{
+    url: string,
+    className: string
+}
+
+const Avatar:FC<props> = ({url,className}) => {
+  return (
+    <img 
+    loading='lazy'
+    className='h-10 rounded-full cursor-pointer transistion duration-150 transform hover:scale-110 ${className}'
+    src={url}
+    />
+  )
+}
+
+export default Avatar
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { ViewGridIcon } from "@heroicons/react/solid" 
+import Avatar from '../Components/Avatar';
 
 const Home = () => {
   return (
@@ -14,6 +15,8 @@ const Home = () => {
                 <p className='link'>Gmail</p>
                 <p className='link'>Images</p>
                 <ViewGridIcon className="h-10 w-10 p-2 rounded-full hover:bg-gray-100 cursor-pointer" />
+                <Avatar 
+            url="https://img.freepik.com/free-vector/blue-circle-with-white-user_78370-4707.jpg?w=826&t=st=1726683354~exp=1726683954~hmac=ffbaaf6d3fb47ff60970bf5998fb1cfdfc4cac0cde3360a6cd4f043350c71511" className={''}                />
             </div>
         </header>    
     </div>
