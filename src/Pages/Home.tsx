862eed86aa077e87ca76d0a582384e45571fe8b2
dominic <dominicseduraja3@gmail.com>
2024-09-18 23:34:59 +0530

    hero icons added

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { ViewGridIcon } from "@heroicons/react/solid" 
 
 const Home = () => {
   return (
@@ -12,10 +13,11 @@ const Home = () => {
             <div className='flex space-x-4 items-center'>
                 <p className='link'>Gmail</p>
                 <p className='link'>Images</p>
+                <ViewGridIcon className="h-10 w-10 p-2 rounded-full hover:bg-gray-100 cursor-pointer" />
             </div>
         </header>    
     </div>
   )
 }
 
-export default Home
+export default Home;
